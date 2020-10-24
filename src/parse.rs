848ee44c53fad5f5c435d75459ed91360ad255d3
Yury Korchemkin <yuri.korchyomkin@googlemail.com>
2020-10-24 14:41:09 -0700

    expect & sequence_with_sep

---
+++ src/parse.rs
@@ -0,0 +1,29 @@
+pub(crate) fn expect<Token: std::fmt::Debug + PartialEq>(
+    tk: Token,
+    tokens: &[Token],
+) -> Result<&[Token], String> {
+    if tokens[0] == tk {
+        Ok(&tokens[1..])
+    } else {
+        Err(format!("Expected {:?}", tk))
+    }
+}
+
+pub(crate) fn sequence_with_sep<'a, 'b, Token: Clone + std::fmt::Debug + PartialEq, T>(
+    parser: impl Fn(&'b [Token]) -> Result<(&'b [Token], T), String>,
+    sep: Token,
+    mut ts: &'b [Token],
+) -> Result<(&'b [Token], Vec<T>), String> {
+    let mut items = Vec::new();
+
+    while let Ok((new_s, item)) = parser(ts) {
+        items.push(item);
+        match expect(sep.clone(), new_s) {
+            Ok(new_s) => {
+                ts = new_s;
+            }
+            Err(_) => return Ok((new_s, items)),
+        }
+    }
+    Ok((ts, items))
+}
